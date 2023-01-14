af0b03ee2b984f6d0f1b0bd5cbd476a2f56365d4
ali-anapalar <aaanapalarali@gmail.com>
2023-01-15 01:49:27 +0300

    Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+
+
+
+
+enum Days 
+{
+    Monday=1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+enum WeatherForecast 
+{
+    Cold=5,
+    Normal=10,
+    Hot=25,
+    SoHot=30
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Days.Sunday);
+        Console.WriteLine((int)Days.Monday);
+
+        int temperature = 32;
+        if (temperature <= (int)WeatherForecast.Normal) 
+        {
+            Console.WriteLine("Dışarıya çıkmak için biraz daha havanın ısınmasını bekle");
+        }
+        else if (temperature >= (int)WeatherForecast.Hot) 
+        {
+            Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+        }
+        else if (temperature >= (int)WeatherForecast.Normal && temperature < (int)WeatherForecast.SoHot) 
+        {
+            Console.WriteLine("Hadi dışarıya");
+        }
+    }
+}
